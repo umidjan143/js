98e53cb5fa5400e38cc230d5406bce2be7ab9766
umidjan143 <133630002+umidjan143@users.noreply.github.com>
2023-07-11 22:50:40 +0500

    js 2 lesson 1

--- main.js
+++ main.js
@@ -1,519 +1,142 @@
-//1
+// function any(one) {
 
-// var a=5;
-
-// if( a > 0 ) {
-//         console.log("musbat");
-//      }else{
-//         console.log("manfiy");
-//      }
-
-
-
-
-
-//2
-
-
-//  var a=3 ;
-
-//  if ( a%2==0 )
-//  {
-//     console.log("juft");
-//  }else{
-//     console.log("toq");
-//  }
- 
- 
-
-
-
-//3
-
-//  var a=3 ;
-
-//  if ( a%2!=0 )
-//  {
-//     console.log("toq");
-//  }else{
-//     console.log("juft");
-//  }
-
-
-
-
-//4
-
-//  var a=5
-//  var b=7
-//  if( a > 0 && b>0 ) {
-//          console.log("musbat");
-//       }else{
-//          console.log("manfiy");
-//       }
-
-
-
-
-
-// //5
-
-// var a=5;
-// var b=4;
-// var c=3;
-
-// if (a>b && b>c)
-// {console.log(true);}
-// else{console.log(false);}
-
-
-
-
-
-//6
-
-// var a=5;
-// var b=4;
-// var c=3;
-// if (a>b && b>c || c>b && b > a)
-// {console.log(true);}
-// else{console.log(false);}
-
-
-
-
-//7
-
-// var a=5;
-// var a=7;
-
-// if( a%2 != 0 && b%2 != 0) {
-//     console.log(toq);
-// }else {console.log(toq emas);}
-
-
-
-
-// //8
-
-// var a=1;
-// var b=2;
-// var c=3;
-// var d=4;
-
-// if (a>0 && b>0 && c>0 && d>0 ) {
-// console.log(true);    
-// } else {console.log(false) ;
+//     return one()
     
 // }
 
 
-//9
+// function first() {
 
-// var a=1;
-// var b=2;
-// var c=-1;
-
-// if (a>0 && b> 0 && c<0 || b>0 && c> 0 && a<0 || a>0 && c> 0 && b<0) {
-// console.log(true);    
-// } else {console.log(false);}
-
-
-
-
-//10
-
-
-
-// var a=7
-
-// switch (a) {
-//     case 1 : 
-//         console.log("monday");
-//         break ;
-        
-//     case 2 : 
-//         console.log("tuesday");
-//         break ;
-        
-//     case 3 : 
-//         console.log("wednesday");
-//         break ;
-        
-//     case 4 : 
-//         console.log("thursday");
-//         break ;
-        
-//     case 5 : 
-//         console.log("friday");
-//         break ;
-        
-//     case 6 : 
-//         console.log("saturday");
-//         break ;
-        
-        
-        
-//         case 7 : 
-//         console.log("sunday");
-//         break ;
-
-      //     default: console.log(mavjud_emas);
-//         break;
-
-
-        
-
-// }
-
-
-
-
-
-
-//11
-
-
-
-// var a=90
-
-// switch (a) {
-//     case 99 : 
-//         console.log("UZMOBILE");
-//         break ;
-        
-//     case 90 : 
-//         console.log("BEELINE");
-//         break ;
-        
-//     case 93 : 
-//         console.log("UCELL");
-//         break ;
-        
-//     case 91 : 
-//         console.log("BEELINE");
-//         break ;
-        
-//     case 94 : 
-//         console.log("UCELL");
-//         break ;
-        
-//     case 95 : 
-//         console.log("UMS");
-//         break ;
-        
-        
-        
-//         case 88 : 
-//         console.log("UMS");
-//         break ;
-
-        
-//         case 33 : 
-//         console.log("HUMANS");
-//         break ;
-
-        
-        //     default: console.log(mavjud_emas);
-//         break;
-
-
-// }
-
-
-
-
-
-//12
-
-
-
-// var a = 9;
-
-// if (a>0) { console.log(a+1);
-    
-// } else { console.log(a-1);
+//     return console.log("first example");
     
 // }
 
+// function second() {
 
-
-
-//13
-
-
-
-// var a = 9;
-
-// if (a>0) { console.log(a+3);
-    
-// } else { console.log(a-2);
+//     return console.log("2nd example");
     
 // }
 
+// function third () {
 
-
-
-//14 
-
-
-// var a = 4;
-// var b=5;
-
-// if (a>b) { console.log(a);
-    
-// } else { console.log(b);
+//     return console.log("3rd example");
     
 // }
 
+// function add() {
 
-
-
-
-
-//15
-
-
-// var a = 7;
-// var b=6;
-// var c=9;
-
-// if (a>b) { if (a>c) {console.log(a);
-    
-// } else {
-// console.log(c);    
+//     return console.log(9+5);;
 // }
-    
-// } else { if (b>c) { console.log(b);
-    
-// } else {console.log(c);
-    
-// }
+
+// function multiply() {
+
+//     return console.log(9*11);
     
 // }
 
+// function func() {
 
-
-
-//16
-
-
-
-// var a = 7;
-// var b=6;
-// var c=9;
-
-// if (a<b) { if (a<c) {console.log(a);
-    
-// } else {
-// console.log(c);    
-// }
-    
-// } else { if (b<c) { console.log(b);
-    
-// } else {console.log(c);
-    
-// }
+//     return console.log("some func");
     
 // }
 
+// any( second ) ; 
 
 
 
 
-//17 
 
 
-// var jins=m;
 
-// switch (jins) {
-//     case m : console.log(erkak);
-        
-//         break;
 
-// case f : console.log(ayol);
 
-// break;
-//     default: console.log(mavjud_emas);
-//         break;
-// }
 
 
 
 
-//18 
 
-// var baho = 0;
-// switch (baho) {
-//     case 5 : console.log("500.000 so'm");
-     
-//         break;
-// case 4 : console.log("400.000 so'm");
-// break;
 
-// case 3 : console.log("300.000 so'm");
-// break;
 
-//          }
 
 
 
 
 
 
-//19
 
 
 
-// var a=-6;
 
-// if( a > 0 ) {
-//         if (a%2==0) {console.log("juftMusbat");
-            
-//         } else { console.log("toqMusbat");
-            
-//         }
-//      }else{
-//         if (a%2==0) { console.log("juftManfiy");
-            
-//         } else {
-//             console.log("toqManfiy");
-//         }
-//      }
 
 
 
 
 
-//20
 
-// var year=2005 ; 
-// var age=2023-year ;
 
-// console.log(age);
 
 
 
+// // function add(x, y, myfunc) {
+// //   let res = x + y;
+// //   return myfunc(res);
+// // }
 
-//21
+// // function newFunc(some) {
+// //   return console.log(some);
+// // }
 
+// // add(4, 5, newFunc);
 
-// var A=5;
-// var B=5;
-// var C=5;
+// // const myTimeout = setTimeout(myGreeting, 5000);
+// // function myGreeting() {
+// //   console.log("hello");
+// // }
+// // function myStopFunction() {
 
-// if (A>=B|| B>=C) { console.log("true");
-    
-// } else { console.log("false");
-    
-// }
+// //     clearTimeout(myTimeout);
+// // console.log('stopped');
+// // }
 
 
 
 
-//22
 
-// var a=5;
-// var b=6;
 
-// if (a%2!=0 && b%2==0 || a%2==0 && b%2!=0  )  { console.log("true");
-    
-// } else {console.log("false");
-    
-// }
 
+// // let time = new Date() ; 
+// // let sekund = time.getSeconds()
 
 
+// // function logSmth() {
+// //     sekund++;
+// //     console.log(sekund);
 
-//23
 
-// var a = 7;
-//  var b=5;
-// var c=9;
+// // }
 
-// if (a>0 && b>0 && c>0) { console.log("true");
-    
-// } else {console.log("false");
-    
-// }
+// // let inter=setInterval(logSmth , 100)
+// // function stopinterval() {
+// // clearInterval(inter)    
+// // } ;
 
 
 
 
 
-//24
 
 
-// var a = 7;
-//  var b=-1;
-// var c=-9;
 
-// if (a>0 && b<0 && c<0 || b>0 && a<0 && c<0 || c>0 && b<0 && a<0)  {console.log("true");
-    
-// } else {console.log("false");
-    
-// }
 
 
 
 
 
-//25
 
-//-
 
 
 
-//26 
 
-//-
 
 
 
-
-
-
-
-
-
-
-//27
-
-
-
-// var laptop="asus"
-
-// switch (laptop) {
-//     case "hp" : 
-//         console.log("$500");
-//         break ;
-        
-//     case "victus" : 
-//         console.log("$600");
-//         break ;
-        
-//     case 'lenovo' : 
-//         console.log("$300");
-//         break ;
-        
-//     case 'acer' : 
-//         console.log("$400");
-//         break ;
-        
-//     case 'asus' : 
-//         console.log("$500");
-//         break ;
-  
-//           default: console.log(mavjud_emas);
-//         break;
-
-
-        
-
-// }
-
-
-
-//28
-
-//-
