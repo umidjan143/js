df3831ecafd733d8cbc679425d5e2f319090e8df
umidjan143 <133630002+umidjan143@users.noreply.github.com>
2023-07-11 22:53:38 +0500

    Add files via upload

---
+++ lesson1/main.js
@@ -0,0 +1,142 @@
+// function any(one) {
+
+//     return one()
+    
+// }
+
+
+// function first() {
+
+//     return console.log("first example");
+    
+// }
+
+// function second() {
+
+//     return console.log("2nd example");
+    
+// }
+
+// function third () {
+
+//     return console.log("3rd example");
+    
+// }
+
+// function add() {
+
+//     return console.log(9+5);;
+// }
+
+// function multiply() {
+
+//     return console.log(9*11);
+    
+// }
+
+// function func() {
+
+//     return console.log("some func");
+    
+// }
+
+// any( second ) ; 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // function add(x, y, myfunc) {
+// //   let res = x + y;
+// //   return myfunc(res);
+// // }
+
+// // function newFunc(some) {
+// //   return console.log(some);
+// // }
+
+// // add(4, 5, newFunc);
+
+// // const myTimeout = setTimeout(myGreeting, 5000);
+// // function myGreeting() {
+// //   console.log("hello");
+// // }
+// // function myStopFunction() {
+
+// //     clearTimeout(myTimeout);
+// // console.log('stopped');
+// // }
+
+
+
+
+
+
+
+// // let time = new Date() ; 
+// // let sekund = time.getSeconds()
+
+
+// // function logSmth() {
+// //     sekund++;
+// //     console.log(sekund);
+
+
+// // }
+
+// // let inter=setInterval(logSmth , 100)
+// // function stopinterval() {
+// // clearInterval(inter)    
+// // } ;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
